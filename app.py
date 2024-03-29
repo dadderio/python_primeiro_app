41ce518193a65690ee7dff36dbf01df940ad4e38
dadderio <dadderio79@outlook.com>
2024-03-28 23:02:11 -0300

    update

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ def finalizar_app():
     #os.system('clear) no mac
     print('Finalizando o app')
 
+def opcao_invalida():
+    print('Opção inválida! \n')
+    input('Digite uma tecla para voltar ao menu principals')
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair \n')
+  
 def escolher_opcao():
     opcao_escolhida = int(input('Escolha uma opção: '))
     # opcao_escolhida = int(opcao_escolhida)
@@ -25,14 +35,12 @@ def escolher_opcao():
         print('Listar restaurantes')
     elif opcao_escolhida == 3:
         print('Ativar restaurante')
-    else:
+    elif opcao_escolhida == 4:
         finalizar_app()
+    else:
+        opcao_invalida()
 
-def exibir_opcoes():
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurante')
-    print('3. Ativar restaurante')
-    print('4. Sair\n')
+
 
 def main():
     exibir_nome_do_programa()
