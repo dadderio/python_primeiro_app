6cc7b7d9e276a77b05462b3745e3c1828cc36de7
dadderio <dadderio79@outlook.com>
2024-03-30 11:37:14 -0300

    update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -17,7 +19,8 @@ def finalizar_app():
 
 def opcao_invalida():
     print('Opção inválida! \n')
-    input('Digite uma tecla para voltar ao menu principals')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
@@ -25,24 +28,44 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair \n')
   
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes \n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:  ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso! \n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else:
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes: \n')
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+    input('\n Digite uma tecla para voltar ao menu principal')
+    main()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
         opcao_invalida()
 
 
-
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
---
+++ for/for.py
@@ -0,0 +1,11 @@
+#numero = -1
+#for _ in range(3):  # Supondo um número máximo de tentativas (3) arbitrário
+ #   numero = int(input("Digite um número positivo: "))
+  #  if numero > 0:
+   #     break
+
+#print("Você digitou:", numero)
+
+for i in range(10):
+    print(i)
+    i = 5  
